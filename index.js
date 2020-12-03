e3e09ae33a000ee4227cf35f6e26f14300577023
root <root@ip-172-31-76-249.ec2.internal>
2020-12-03 10:59:57 +0000

    modified index code

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express =require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+var DESTINATION_VERIFICATION_TOKEN = 'abcd01234';
+
 app.use(bodyParser.json());
 
 app.listen(80 , function() {
@@ -11,4 +13,20 @@ console.log('Server started . Listening to port 80');
 
 app.get('/' , function(req,res){
 res.send('Hello world!!!!');
+});
+
+
+app.get('/destination' , function(req,res){
+if( req.headers['verification-token'] === DESTINATION_VERIFICATION_TOKEN){
+console.log('verification-token matched!');
+return res.send(req.query.challange);
+}
+
+console.log('verification-token did not match!');
+res.sendStatus(400);
+});
+
+app.post('/destination',function(req,res){
+console.log(req.body);
+res.sendStatus(200);
 });
